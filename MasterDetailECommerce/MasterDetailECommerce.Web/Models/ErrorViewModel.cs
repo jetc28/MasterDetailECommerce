bba5667d74cf56b2da30a8b6861a3a10215cc1ef
Jeremy Toscano Chipana <jeremytoscanoc@gmail.com>
2020-10-03 00:31:45 -0500

    Initialized

---
+++ MasterDetailECommerce/MasterDetailECommerce.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MasterDetailECommerce.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ MasterDetailECommerce/MasterDetailECommerce.Web/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MasterDetailECommerce.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime Date { get; set; }
+
+        public int Stock { get; set; }
+    }
+}
